4f62705357284fab875c5e6827470972185a902f
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-11-06 19:55:41 -0600

    Initial

---
+++ scripts/deployArgs.js
@@ -0,0 +1,24 @@
+module.exports = [
+ 'Numb Bunny NFT',
+ 'NBNFT',
+ 
+    '0x9C3f261e2cc4C88DfaC56A5B46cdbf767eE2f231',
+    '0xc01aC67ba14F13Af4fCD5b4ba3784F39a703cda0',
+    'https://public-pre-ipfs.s3.amazonaws.com/NumbBunnyNFT/json/',
+    'https://public-pre-ipfs.s3.amazonaws.com/NumbBunnyNFT/assets/Unrevealed.json',
+
+    [
+        ethers.utils.getAddress('0xf07839FAa0a863313bf217da333ac4d67aaB5923'),
+        ethers.utils.getAddress('0xF0A1A510191B3563F924C92B76ADD13Dc7CDD78a'),
+        ethers.utils.getAddress('0xa793EBC07b3c44123933c3e4e2b0A7807FEE8257'),
+        ethers.utils.getAddress('0x36CD5Dc24234332B3B7B2DC77AbC57242a05B7c9'),
+        ethers.utils.getAddress('0x8912e40d291693C7A86C418D52b46766780C6989')
+    ],
+    [
+        40,
+        35,
+        5,
+        5,
+        15
+    ]
+]
